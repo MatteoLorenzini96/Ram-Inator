291151996269cb899cc4b9ecfac47711600c1a23
maxmat888 <massimotricoli031@gmail.com>
2025-01-07 17:34:24 +0100

    fix 4 (spero ultimo)

--- Assets/Scripts/Manager/TurnManager.cs
+++ Assets/Scripts/Manager/TurnManager.cs
@@ -131,40 +131,52 @@ public class TurnManager : MonoBehaviour
 
     private void EvaluateObjects()
     {        
-         // Calcola la percentuale di oggetti rimanenti
-        int remainingObjects = objectsWithCollisionStateChanger.Count;
-        float percentage = (float)remainingObjects / initialObjectCount * 100f;
+       int remainingObjects = objectsWithCollisionStateChanger.Count;
+    float percentage = (float)remainingObjects / initialObjectCount * 100f;
 
-        // Valutazione basata sui tentativi e sugli oggetti rimasti
-        if (remainingAttempts > twoStarsAttempts && remainingAttempts < maxAttempts)
-        {
-            // 100% completato
-            stars = 1;
-        if (remainingAttempts >= threeStarsAttempts)
+    // Valutazione basata sui tentativi e sugli oggetti rimasti
+    if (percentage == 0f)
+    {
+        // Completato al 100% (tutti gli oggetti distrutti)
+        if (remainingAttempts <= threeStarsAttempts)
         {
             // Usa meno tentativi del massimo per 3 stelle
             stars = 3;
             Debug.Log("3 stelle");
         }
-        else if (remainingAttempts >= twoStarsAttempts)
+        else if (remainingAttempts <= twoStarsAttempts)
         {
             // Usa meno tentativi del massimo per 2 stelle
             stars = 2;
             Debug.Log("2 stelle");
         }
+        else if (remainingAttempts > twoStarsAttempts && remainingAttempts < maxAttempts)
+        {
+            // Usa più tentativi di quelli previsti per 2 stelle ma meno del massimo
+            stars = 1;
+            Debug.Log("1 stella - Tentativi superiori a quelli per 2 stelle, ma inferiori al massimo");
+        }
         else
         {
-            // Non raggiunge la soglia minima
+            // Usa più tentativi del massimo
             stars = 0;
+            Debug.Log("1 stelle - Tentativi superiori al massimo");
         }
-        }
-
-        ActivatePanel();
-
-        // Salva il punteggio nel LevelResultsManager
-
-        LevelResultsManager.Instance.SaveLevelResult(SceneManager.GetActiveScene().buildIndex, stars);
     }
+    else
+    {
+        // Se non hai completato il livello (ci sono ancora oggetti da distruggere)
+        stars = 0;  // Completamento parziale, quindi solo 1 stella
+        Debug.Log("0 stella - Completamento parziale");
+    }
+    
+
+    ActivatePanel();
+
+    // Salva il punteggio nel LevelResultsManager
+    LevelResultsManager.Instance.SaveLevelResult(SceneManager.GetActiveScene().buildIndex, stars);
+}
+
 
     private void ActivatePanel()
     {

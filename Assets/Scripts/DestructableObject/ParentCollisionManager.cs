ad988e92c18fa8f103be338ad4d6f3b4ddc1cfec
Matteo Lorenzini <matteolorenzini96.ml@gmail.com>
2025-01-09 16:57:13 +0100

    UPDATE Bricks now working 2
    Aggiunta la logica per tutti i frammenti di rimbalzare sui muri e venire sparati via dall'effetto della Palla ad Impatto

--- Assets/Scripts/DestructableObject/ParentCollisionManager.cs
+++ Assets/Scripts/DestructableObject/ParentCollisionManager.cs
@@ -1,12 +1,17 @@
+using System.Collections;
 using UnityEngine;
 
 public class ParentCollisionManager : MonoBehaviour
 {
     public float forza = 10f; // La forza da applicare ai frammenti
+    private float timeToReset = 1f; // Tempo in secondi prima di ripristinare il trigger
+
 
     // Funzione pubblica che gestisce i trigger
     public void GestisciTrigger(Collider other, MonoBehaviour figlio)
     {
+        //Debug.Log("Tag dell'oggetto che entra nel trigger: " + other.tag);
+
         // Se l'oggetto che entra nel trigger è la "Palla"
         if (other.CompareTag("Palla"))
         {
@@ -22,8 +27,23 @@ public class ParentCollisionManager : MonoBehaviour
 
                 // Applica la forza nella direzione calcolata
                 rb.AddForce(direzione * forza, ForceMode.Impulse);
+                //Debug.Log("Forza Applicata");
             }
         }
+
+        // Se l'oggetto che entra nel trigger è un "Muro"
+        if (other.CompareTag("Metallo"))
+        {
+            Collider figlioCollider = figlio.GetComponent<Collider>();
+            if (figlioCollider != null)
+            {
+                figlioCollider.isTrigger = false;
+
+                // Avvia la coroutine per riabilitare il trigger dopo un certo tempo
+                StartCoroutine(ResetTrigger(figlioCollider));
+            }
+        }
+
         // Se l'oggetto che entra nel trigger è un "Fragment"
         else if (other.CompareTag("Fragment"))
         {
@@ -44,6 +64,7 @@ public class ParentCollisionManager : MonoBehaviour
 
                     // Applica la forza nella direzione calcolata
                     rb.AddForce(direzione * forza, ForceMode.Impulse);
+                    //Debug.Log("Forza dei Fragment applicata");
                 }
             }
         }
@@ -55,4 +76,20 @@ public class ParentCollisionManager : MonoBehaviour
         MonoBehaviour figlio = GetComponent<MonoBehaviour>(); // Ottieni il MonoBehaviour associato
         GestisciTrigger(other, figlio); // Chiamata corretta con due argomenti
     }
+
+    // Coroutine per ripristinare il trigger dopo un certo tempo
+    IEnumerator ResetTrigger(Collider collider)
+    {
+        //Debug.Log("Coroutine Attiva");
+
+        // Aspetta per il tempo specificato
+        yield return new WaitForSeconds(timeToReset);
+
+        // Ripristina il trigger
+        if (collider != null)
+        {
+            collider.isTrigger = true;
+        }
+    }
+
 }
